1fa778749c82b3350a2f84bbf855cd4c5c9a6e2e
andreathomas10 <104719647+andreathomas10@users.noreply.github.com>
2024-03-18 09:13:56 +0100

    hello

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
+
 import gradio as gr
+#import unstructured as us
+print("Hello world")
+def is_cv(file):
+    # pass file through parser
+    # pass text to LLM
+    response = "Work in"
+    return response
 
-def greet(name):
-    return "Hello " + name
+title = "CV Scanner"
+demo = gr.Interface(fn = is_cv, inputs= [gr.File()], outputs = ["text"], title = title, 
+                    description="DISCLAIMER: This is only a Proof-of-Concept demonstration.",
+                    article="Please upload the pdf to be scanned.")
 
-demo = gr.Interface(fn = greet, inputs= ["text"], outputs = ["text"])
+demo.queue().launch(share = True)
 
-demo.launch(share = True)
+
+# with blocks
+css = """ h1 {
+    text-align: center;
+    display:block;} """
+
+with gr.Blocks(css=css) as demo2:
+    gr.Markdown("CV Scanner"),
+    inputs= [gr.File()],
+    outputs = ["text"]
